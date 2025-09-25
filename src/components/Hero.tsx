802176808e3be864a294411d1f0e95cb5f6c8aa0
m4max <m4max@goodboys.club>
2025-09-25 18:09:49 +0200

    refactor: adjust spacing in Hero component for improved readability

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,13 +9,13 @@ export const Hero = () => {
           <img src={image} alt="Hero" className="w-1/2 mx-auto" />
         </main>
 
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto text-balance">
+        <p className="text-xl mt-8 text-muted-foreground md:w-10/12 mx-auto text-balance">
           Daily&nbsp;cagechecks. 31&nbsp;days&nbsp;locked.
           Rewards&nbsp;if&nbsp;you&nbsp;last. Just&nbsp;$1/day.
           Sign&nbsp;up&nbsp;now.
         </p>
 
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto text-balance">
+        <p className="text-xl mt-8 text-muted-foreground md:w-10/12 mx-auto text-balance">
           <Link to="/#join-the-challenge">
             ⬇️ Sign up for the Locktober Challenge before October 14th ⬇️
           </Link>
